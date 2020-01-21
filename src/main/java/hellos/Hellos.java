3a0df4aacceb632b1ba1c6ac8f8a1e2df46a0cb8
Benjamin Burgess <burge337@morris.umn.edu>
2020-01-21 14:19:26 -0600

    Added hello functions for Benjamin and Joe
    Yep, that's what we did!

--- src/main/java/hellos/Hellos.java
+++ src/main/java/hellos/Hellos.java
@@ -17,10 +17,20 @@ public class Hellos {
     StringBuilder builder = new StringBuilder();
 
     builder.append(WELCOME_LINE + "\n");
+    builder.append(benjaminSaysHello());
+    builder.append(joeSaysHello());
     builder.append(kkSaysHello());
     builder.append(nicSaysHello());
 
     return builder.toString();
+  }
+
+  private static String benjaminSaysHello() {
+    return "Benjamin says 'Howdy!'\n";
+  }
+
+  private static String joeSaysHello() {
+    return "Joe says 'Hello hello!'\n";
   }
 
   private static String nicSaysHello() {
